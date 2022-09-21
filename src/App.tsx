6ec9e1683a82293eeecfb3aec316ce31d3cbfe5b
mikapoohusagi <marshanachan87@gmail.com>
2022-09-21 16:00:08 +0900

    コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count,setCount]=useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p data-testid="count">{count}</p>
+        <button type='button' data-testid="count-up-button" onClick={()=>setCount((count)=>count+1)}>countUp</button>
         <p>
-          Editaaa <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
