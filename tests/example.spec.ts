22992fba18f94c5254b3adba3d6e0bf9ae2fc73b
mikapoohusagi <marshanachan87@gmail.com>
2022-09-22 11:54:33 +0900

    スクショ先変更

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 
 test('visual regression test', async ({ page }) => {
-  await page.goto('http://localhost:3000/top', { waitUntil: 'networkidle' });
+  await page.goto('https://playwright.dev/', { waitUntil: 'networkidle' });
   await page.screenshot({ path: '__screenshots__/top.png', fullPage: true });
 });
